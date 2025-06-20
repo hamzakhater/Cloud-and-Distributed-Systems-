0814b52c7335bb4cab122d23de31265b2e1c1ebd
unknown <hamzakhater69@gmail.com>
2025-06-20 14:17:19 +0300

    Update

--- models/fileModel.js
+++ models/fileModel.js
@@ -34,35 +34,3 @@ module.exports = {
   listFiles,
   extractText,
 };
-
-// const fs = require("fs");
-// const path = require("path");
-// const mammoth = require("mammoth");
-// const pdfParse = require("pdf-parse");
-
-// const uploadsDir = path.join(__dirname, "..", "uploads");
-
-// const readFiles = async () => {
-//   return fs.promises.readdir(uploadsDir);
-// };
-
-// const extractTextFromFile = async (filename) => {
-//   const filePath = path.join(uploadsDir, filename);
-//   if (!fs.existsSync(filePath)) throw new Error("File not found");
-
-//   if (filename.endsWith(".pdf")) {
-//     const dataBuffer = await fs.promises.readFile(filePath);
-//     const data = await pdfParse(dataBuffer);
-//     return data.text;
-//   } else if (filename.endsWith(".docx")) {
-//     const result = await mammoth.extractRawText({ path: filePath });
-//     return result.value;
-//   } else {
-//     throw new Error("Unsupported file type");
-//   }
-// };
-
-// module.exports = {
-//   readFiles,
-//   extractTextFromFile,
-// };
